c947d07f45c8ceceee501b47f3cc912c1f00252a
Gaurav Pant <gauravpgaurav@gmail.com>
2016-03-10 11:17:28 +0530

    Rajesh Khanna

---
+++ SpringWithAnnotations/src/com/training/domains/Doctor.java
@@ -0,0 +1,41 @@
+package com.training.domains;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("ent")
+public class Doctor {
+
+	@Value("Gaurav Pant")
+	private String docName;
+
+	@Value("9740591223")
+	private long phoneNumber;
+
+	public Doctor() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getDocName() {
+		return docName;
+	}
+
+	public void setDocName(String docName) {
+		this.docName = docName;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Doctor [docName=" + docName + ", phoneNumber=" + phoneNumber + "]";
+	}
+
+}
