bbdf6886097f9c2b880cd205b38e69c32f10de0e
Gaurav Pant <gauravpgaurav@gmail.com>
2016-03-10 13:02:03 +0530

    JDBC Works

---
+++ SpringJDBC/src/com/training/apps/Application.java
@@ -0,0 +1,26 @@
+package com.training.apps;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.training.daos.InvoiceDaoImpl;
+import com.training.entity.Invoice;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("bean.xml");
+		InvoiceDaoImpl obj = ctx.getBean("dao", InvoiceDaoImpl.class);
+		Invoice inv = ctx.getBean("invoice", Invoice.class);
+
+		inv.setInvoiceNumber(201);
+		inv.setCustomerName("Ramesh");
+		inv.setAmount(12.0);
+
+		boolean result = obj.add(inv);
+		System.out.println("Row Added : " + result);
+
+	}
+
+}
