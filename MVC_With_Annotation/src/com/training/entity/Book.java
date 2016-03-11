9856664e35764761df4f3febb34758aeed14a03f
Gaurav Pant <gauravpgaurav@gmail.com>
2016-03-11 13:22:14 +0530

    MVC_With_Annot

---
+++ MVC_With_Annotation/src/com/training/entity/Book.java
@@ -0,0 +1,32 @@
+package com.training.entity;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Book {
+
+	private long bookNumber;
+	private String bookName;
+
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public long getBookNumber() {
+		return bookNumber;
+	}
+
+	public void setBookNumber(long bookNumber) {
+		this.bookNumber = bookNumber;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+}
