3734d59378e6e3a47c72ac5b0bd46ec380c3e301
Gaurav Pant <gauravpgaurav@gmail.com>
2016-03-15 12:27:37 +0530

    Default

---
+++ FirstBookProject/src/main/java/com/example/FirstBookProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBookProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBookProjectApplication.class, args);
+	}
+}
